68d45498404f0aaff88947bb6314f0557a81b3de
zzik <edin.base@outlook.com>
2022-12-05 08:12:26 +0100

    initial logic and layout setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
-import MovieCard from './components/MovieCard'
+import { createContext, useContext, useState } from "react"
+import { Content } from "./components/Content"
+import { SearchBar } from "./components/SearchBar"
+import { TypeControl } from "./components/TypeControl"
+
+interface Props {
+  is: string,
+  isNot: string
+}
+
+export const TypeContext = createContext<Props | null>(null)
+export const SearchContext = createContext<string | null>('')
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [movies, setMovies] = useState(true)
+  const [series, setSeries] = useState(false)
+  const handleType = () => {
+    setMovies(!movies)
+    setSeries(!series)
+  }
+
+  const [query, setQuery] = useState('')
+  const handleQuery = (input: string) => {
+    setQuery(input)
+  }
 
   return (
-    <div className="App">
-      <MovieCard text='huh'/>
-    </div>
+    <TypeContext.Provider value={movies ? { is: 'Movies', isNot: 'TV Series' } : { is: 'TV Series', isNot: 'Movies' }}>
+      <div className="App">
+        <TypeControl toggleContentType={handleType} />
+        <SearchBar searchPhrase={(e) => handleQuery(e.target.value)} />
+        <Content payload={query} />
+      </div>
+    </TypeContext.Provider>
   )
 }
 
