5a372138572fa7c4862976275b609f8a1823d0d5
zzik <edin.base@outlook.com>
2022-12-12 16:33:03 +0100

    replacement file for environment variables since dotenv doesnt work

---
+++ src/components/utils/index.ts
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+
+export const test = {
+    url : 'https://api.themoviedb.org/3/',
+    apiKey : '?api_key=516bd90eaa5d6fc72dd79e7ba1bfaa28',
+    searchStatusQuery : 'search/',
+    searchQuery :`&query=`
+}
+
