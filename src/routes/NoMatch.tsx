ab970b7d14a4d6f8f33b3bcebfb90424dcf60949
zzik <edin.base@outlook.com>
2022-12-13 12:21:04 +0100

    customized 404 message

--- src/routes/NoMatch.tsx
+++ src/routes/NoMatch.tsx
@@ -5,7 +5,7 @@ function NoMatch() {
       <div>
         <h2>Nothing to see here!</h2>
         <p>
-          <Link to="/">Go to the home page</Link>
+          <Link to="/">Go home.</Link>
         </p>
       </div>
     );
