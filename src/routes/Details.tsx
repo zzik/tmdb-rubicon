152b11453cbabcfe5378b0878e96a4a46de3db41
zzik <edin.base@outlook.com>
2022-12-13 12:27:36 +0100

    improved navigation logic

--- src/routes/Details.tsx
+++ src/routes/Details.tsx
@@ -1,29 +1,20 @@
-import React from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import { DetailsCard } from '../components/DetailsCard'
+import React, { useContext } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import { environment } from '../components/utils'
 import useDetails from '../components/hooks/useDetails'
 import useVideo from '../components/hooks/useVideo'
-
-
+import { SearchContext, TypeContext } from '../components/context'
 
 export const Details = () => {
 
-    let base = environment.imageUrl
-    const navigate = useNavigate()
     const prms = useParams()
     const deets = useDetails(prms)
-    let trailer = useVideo(prms.contype, prms.id)
 
     if (deets) {
-        // let link = deets.backdrop_path ? deets.backdrop_path : deets.poster_path
-        // console.log(link)
-
         return <div>
-            <button onClick={() => navigate(-1)}>YES</button>
+            <Link to={'/'} className='back-button'>BACK</Link>
             <div className='visual-content'>
                 {/* {trailer.link ? <iframe src={trailer.link} title={trailer.name} ></iframe> : <img src={link ? base + 'w1280' + link : 'null'} />} */}
-
             </div>
 
             <h1>{deets.name ? deets.name : deets.title}</h1>
