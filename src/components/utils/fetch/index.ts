00388a45a1b1f14a2c78a24ce5bf1af221cf6a24
zzik <edin.base@outlook.com>
2022-12-13 08:54:22 +0100

    lumping components with fetching logic

---
+++ src/components/utils/fetch/index.ts
@@ -0,0 +1,7 @@
+import { fetchCollection } from "./collection";
+import { fetchItem } from "./item";
+import { fetchVideos } from "./videos";
+import { fetchImageLinks } from "./imageLinks";
+import { fetchTopRated } from "./topRated";
+
+export { fetchCollection, fetchItem, fetchVideos, fetchImageLinks, fetchTopRated }
