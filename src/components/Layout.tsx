d560a61ef8fab1b3d9dab88588c85ab1b6f9f212
zzik <edin.base@outlook.com>
2022-12-13 12:23:45 +0100

    removed unneccessary imports

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Controls } from './Controls'
 import { Display } from './Display'
-import { SearchBar } from './SearchBar'
-import { TypeControl } from './TypeControl'
 
 export const Layout: React.FC = () => {
     return (
