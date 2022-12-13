0dc8e9c9ff43a5892d8de94d176be8f518963ad4
zzik <edin.base@outlook.com>
2022-12-13 09:19:51 +0100

    implementing react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
+import { Routes, Route, Outlet, Link, BrowserRouter } from "react-router-dom";
+import { Layout } from './components/Layout';
+import NoMatch from './routes/NoMatch';
+import { Details } from './routes/Details';
+import Item from './routes/Item';
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Layout />} />
+          <Route path=':contype/:id' element={<Details />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
