9823d8177a2597b5137d51da959c85803080bd55
zzik <edin.base@outlook.com>
2022-12-13 12:26:11 +0100

    code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 
 import './styles/index.css'
 import { useState } from "react"
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { SearchContext, TypeContext, SearchStatusContext } from './components/context'
-
-import { Display } from "./components/Display"
-import { Controls } from './components/Controls'
-import NoMatch from './routes/NoMatch';
-import { Layout } from './components/Layout';
 
 function App() {
 
-  const [currentType, setCurrentType] = useState('movie'), [searchStatus, setSearchStatus] = useState(false), [currentSearch, setCurrentSearch] = useState('')
+  const [currentType, setCurrentType] = useState('tv'), [searchStatus, setSearchStatus] = useState(false), [currentSearch, setCurrentSearch] = useState('')
 
   return (
     <div className="App">
