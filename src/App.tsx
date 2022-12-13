ecbda176a11fa143aaad93cce6580f08c20ba15f
zzik <edin.base@outlook.com>
2022-12-13 09:22:04 +0100

    useCtx improvement, huge overhaul and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,28 @@
-import { createContext, useContext, useState } from "react"
-import { Content } from "./components/Content"
-import { SearchBar } from "./components/SearchBar"
-import { TypeControl } from "./components/TypeControl"
 
-interface Props {
-  is: string,
-  isNot: string
-}
+import './styles/index.css'
+import { useState } from "react"
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { SearchContext, TypeContext, SearchStatusContext } from './components/context'
 
-export const TypeContext = createContext<Props | null>(null)
-export const SearchContext = createContext<string | null>('')
+import { Display } from "./components/Display"
+import { Controls } from './components/Controls'
+import NoMatch from './routes/NoMatch';
+import { Layout } from './components/Layout';
 
 function App() {
 
-  const [movies, setMovies] = useState(true)
-  const [series, setSeries] = useState(false)
-  const handleType = () => {
-    setMovies(!movies)
-    setSeries(!series)
-  }
-
-  const [query, setQuery] = useState('')
-  const handleQuery = (input: string) => {
-    setQuery(input)
-  }
+  const [currentType, setCurrentType] = useState('movie'), [searchStatus, setSearchStatus] = useState(false), [currentSearch, setCurrentSearch] = useState('')
 
   return (
-    <TypeContext.Provider value={movies ? { is: 'Movies', isNot: 'TV Series' } : { is: 'TV Series', isNot: 'Movies' }}>
-      <div className="App">
-        <TypeControl toggleContentType={handleType} />
-        <SearchBar searchPhrase={(e) => handleQuery(e.target.value)} />
-        <Content payload={query} />
-      </div>
-    </TypeContext.Provider>
+    <div className="App">
+      <TypeContext.Provider value={{ contentType: currentType, setContentType: setCurrentType }}>
+        <SearchContext.Provider value={{ searchPhrase: currentSearch, setSearchPhrase: setCurrentSearch }}>
+          <SearchStatusContext.Provider value={{ searchStatus, setSearchStatus }}>
+            <Outlet />
+          </SearchStatusContext.Provider>
+        </SearchContext.Provider>
+      </TypeContext.Provider>
+    </div>
   )
 }
 
