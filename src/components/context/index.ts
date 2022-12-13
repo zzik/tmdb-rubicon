b5c766895594e1df1780ce84e8e4d8d9bc4407cd
zzik <edin.base@outlook.com>
2022-12-13 09:08:52 +0100

    context lump

---
+++ src/components/context/index.ts
@@ -0,0 +1,5 @@
+import { SearchContext } from "./SearchContext";
+import { SearchStatusContext } from "./SearchStatusContext";
+import { TypeContext } from "./TypeContext";
+
+export {SearchContext, SearchStatusContext, TypeContext}
