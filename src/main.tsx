a6196d5d0de60468cbb555679a7667d5eec97629
zzik <edin.base@outlook.com>
2022-12-13 12:25:54 +0100

    cleaning up the code

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { Routes, Route, Outlet, Link, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Layout } from './components/Layout';
 import NoMatch from './routes/NoMatch';
 import { Details } from './routes/Details';
-import Item from './routes/Item';
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// )
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
