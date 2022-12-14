c58d4f5015435350bcb30fa6768d6153e84d9c97
zzik <edin.base@outlook.com>
2022-12-14 09:01:57 +0100

    implementing dynamic css from mockup

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,7 +4,7 @@ import { SearchContext, SearchStatusContext } from './context'
 export const SearchBar: React.FC = () => {
 
     const { searchPhrase, setSearchPhrase } = useContext(SearchContext)
-    const { setSearchStatus } = useContext(SearchStatusContext)
+    const { searchStatus, setSearchStatus } = useContext(SearchStatusContext)
 
     const handleSearch = (phrase: string) => {
         setTimeout(() => {
@@ -18,9 +18,10 @@ export const SearchBar: React.FC = () => {
         }, 1000)
     }
 
+    const corners = searchStatus ? 'active-search' : 'inactive-search'
     return (
     <div className='search-bar'>
-        <input
+        <input className={corners}
         type='text' name='search' id='search' placeholder='Search..' defaultValue={searchPhrase}
         onChange={(e) => {
             handleSearch(e.target.value)
